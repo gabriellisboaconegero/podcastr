d4dae5ee6784f55aa62c8635d6add85d1f3a0abd
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2021-04-24 11:18:19 -0300

    etendendo o uso do getStaticPaths

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,16 @@
 // vai ser chamado apenas uma vez, quando entrar no site
 
-
+import React from 'react';
 import Document, {Html, Head, Main, NextScript} from "next/document";
 
 export default class MyDocument extends Document{
-    render(){
+    render(): React.ReactElement{
         return (
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />               
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+                    <link rel="icon" href="/favicon.png" type="image/png"/>             
                 </Head>
                 <body>
                     <Main />
--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -29,6 +29,10 @@ export default function Episode({episode}: EpisodeProps){
 
     // const router = useRouter();
 
+    // if (router.isFallback){
+    //     return <p>Carregando...</p>
+    // }
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
@@ -60,10 +64,37 @@ export default function Episode({episode}: EpisodeProps){
     );
 }
 
+
+//Como a rota depende de um input ou seja ela é dinamica, o next nã vai gerar uma static page, pois os paths estão vazios
+// caso coloque um path com os parametros que devem ser gerados estaticamentes, aquela rota com o slug vai ser gerada estaticamente na build
+// o fallack determina o comportamento da pagina que não foi gerada estaticamente
+// fallback
+//      false: retorna 404 se a pagia não foi gerada estaticamente, ou seja se a pagina não estiver nos paths, 404
+//      true: se a pagina não foi gerada estaticamente (novamete, não está nos paths) ele vai retornar o conteudo porem a requisição ou seja, aquilo que ocorreria na build se fosse gerada estaticamente vai ser feita pelo browser(client-side).Por rodar no client-side, ao fazer a build o next não vai conseeguir montar a pagina estatica que utiliza essa variavel, pois ele tem um dalay e não esta rodando no server, para consertar isso, usamos o useRouter para verificar se está em um fallback e lidar com isso
+//      blocking: ele faz a requisição e todo o script no server next.js e o usuario só vai ser redirecionado para a pagina depois que o html for gerado nesse server intermediario, diferente do true, que rediceriona para a pagina, faz a requisição e só depois devolve o html
+
+
 export const getStaticPaths: GetStaticPaths = async () => {
+
+    const { data } = await api.get("episodes", {
+        params: {
+          _limit: 2,
+          _sort: 'published_at',
+          _order: 'desc'
+        }
+    });
+
+    const paths = data.map(episode => {
+        return{
+            params: {
+                id: episode.id
+            }
+        } 
+    });
+
     return {
-        paths: [],
-        fallback: 'blocking'
+        paths,
+        fallback: "blocking"
     }
 }
 
